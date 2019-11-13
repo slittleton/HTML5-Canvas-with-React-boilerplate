cceae07f79462cbd08026a7f77f94446d36c89ef
slittleton <littletonltp@gmail.com>
2019-11-13 10:25:10 -0700

    commit

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,39 +2,46 @@ import React, { useEffect, useState, useRef } from "react";
 
 const Canvas = () => {
   const canvasRef = useRef();
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+  // const width = window.innerWidth;
+  // const height = window.innerHeight;
   //====== State ======
   let [mouse, setMouse] = useState({ x: undefined, y: undefined });
+  let [screen, setScreen] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  const { width, height } = screen;
 
   //keep track of xy position of mouse
   const onMouseMove = event => {
     setMouse({ x: event.x, y: event.y });
   };
   // refresh canvas on screen resize
-  const onResize = (canvas, context) => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  const onResize = context => {
+    setScreen({ width: window.innerWidth, height: window.innerHeight });
     init(context); // call draw/animate functions on resize
   };
 
-  
- 
+  useEffect(() => {}, []);
   // ============== useEffect holds all your activity ==============
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    window.addEventListener("resize", () => onResize(canvas, context));
+    window.addEventListener("resize", () => onResize(context));
     window.addEventListener("mousemove", e => onMouseMove(e));
     canvas.width = width;
     canvas.height = height;
 
     // call your drawing/animating functions here
     init(context);
-  }, []);
+  }, [width, height]);
 
   const init = context => {
-    // draw black dot in middle of screen
+    drawDot(context);
+  };
+
+  // draw black dot in middle of screen
+  const drawDot = context => {
     context.beginPath();
     // context.arc(x-position, y-position, radius, startAngle, endAngle, drawCounterClockwise )
     context.arc(width / 2, height / 2, 50, 0, 2 * Math.PI, false);
